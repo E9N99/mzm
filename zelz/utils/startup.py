9f4968c7ea4e812f6595abe7166c9440fe25ae72
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-07-06 05:04:10 +0300

    B2
    B2

--- zelz/utils/startup.py
+++ zelz/utils/startup.py
@@ -1,10 +1,3 @@
-# ZThon
-# Copyright (C) 2022 ZThon . All Rights Reserved
-#< https://t.me/ZThon >
-# This file is a part of < https://github.com/Zed-Thon/ZelZal/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/Zed-Thon/ZelZal/blob/master/LICENSE/>.
-
 import time
 import asyncio
 import importlib
@@ -43,10 +36,9 @@ from .tools import create_supergroup
 ENV = bool(os.environ.get("ENV", False))
 LOGS = logging.getLogger("zelz")
 cmdhr = Config.COMMAND_HAND_LER
-Zed_Vip = (1895219306, 6269975462, 6550930943, 5993018048, 5809896714, 1985225531, 6886550001, 925972505, 6038435721, 5746412340, 1762269116, 6272130846, 1052790413, 6055956182, 5059075331, 6669333713, 6328317500, 5616315677, 6227985448, 232499688, 6608224162, 1719023510, 6748495865, 6612882807, 6093001360, 6470835326, 6831274148, 5616315677)
-Zed_Dev = (1895219306, 925972505, 5746412340, 5003461173, 6227985448, 2095357462, 5176749470, 5426390871, 6269975462, 1985225531, 6550930943, 5003461173, 6227985448, 6269975462, 5746412340, 1850533212, 5616315677, 6470835326, 232499688, 6227985448, 1719023510 , 6801349110, 5280339206, 5616315677, 6470835326)
-zchannel = {"@zthon", "@zed_thon", "@zzzlvv", "@zzzvrr", "@AAAl1l", "@RR_U_RR", "@zzzzzl1I", "@z_zzz8", "@uzz_zz", "@zzkrr", "@zzclll", "@llu_uII", "@X_P_3", "@RRRDB", "@ZThon_Add", "@heroku_error", "@DD_Dii", "@zz_zii", "@AA_Auu", "@TT_lii", "@ZThon1", "@MMM07", "@Scalingo_error", "@Koyeb_Error", "@Koyeb_Errors", "@A_A_9_s", "@artificial_intelligence_4"}
-heroku_api = "https://api.heroku.com"
+Zed_Vip = (7291869416, 9831274148)
+Zed_Dev = (7291869416, 9280339206)
+zchannel = {"@veevvw", "@sedthon_help"}
 if Config.HEROKU_APP_NAME is not None and Config.HEROKU_API_KEY is not None:
     Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
     app = Heroku.app(Config.HEROKU_APP_NAME)
@@ -85,7 +77,7 @@ async def autovars(): #Code by T.me/zzzzl1l
         LOGS.info("تم اضافـة بقيـة الفـارات .. بنجـاح")
 
 
-async def autoname(): #Code by T.me/zzzzl1l
+async def autoname(): 
     if gvarstatus("ALIVE_NAME"):
         return
     await bot.start()
@@ -146,7 +138,7 @@ async def setup_bot():
         sys.exit()
 
 
-async def mybot(): #Code by T.me/zzzzl1l
+async def mybot(): 
     ZELZAL = bot.me.first_name
     Malath = bot.uid
     zel_zal = f"[{ZELZAL}](tg://user?id={Malath})"
@@ -163,7 +155,7 @@ async def mybot(): #Code by T.me/zzzzl1l
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", "ZThon")
+            await bot.send_message("@BotFather", "ماتركـس")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setname")
             await asyncio.sleep(1)
@@ -175,19 +167,19 @@ async def mybot(): #Code by T.me/zzzzl1l
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_file("@BotFather", "zelz/zilzal/logozed.jpg")
+            await bot.send_file("@BotFather", "zelz/Matrix/logosed.jpg")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setabouttext")
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", f"- بـوت زدثــون المسـاعـد ♥️🦾 الخـاص بـ  {bot.me.first_name} ")
+            await bot.send_message("@BotFather", f"- بـوت ماتركـس المسـاعـد ♥️🦾 الخـاص بـ  {bot.me.first_name} ")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setdescription")
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", f"•⎆┊انـا البــوت المسـاعـد الخــاص بـ {zel_zal} \n•⎆┊بـواسطـتـي يمكـنك التواصــل مـع مـالكـي 🧸♥️\n•⎆┊قنـاة السـورس 🌐 @ZThon 🌐")
+            await bot.send_message("@BotFather", f"•⎆┊انـا البــوت المسـاعـد الخــاص بـ {zel_zal} \n•⎆┊بـواسطـتـي يمكـنك التواصــل مـع مـالكـي 🧸♥️\n•⎆┊قنـاة السـورس 🌐 @veevvw 🌐")
         except Exception as e:
             print(e)
 
@@ -205,8 +197,8 @@ async def startupmessage():
             Config.ZEDUBLOGO = await zedub.tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/f821d27af168206b472ad.mp4",
-                caption="**•⎆┊تـم بـدء تشغـيل سـورس زدثــون الخاص بك .. بنجاح 🧸♥️**",
-                buttons=[(Button.url("𝙕𝙏𝙝𝙤𝙣𓅛", "https://t.me/ZThon"),)],
+                caption="**•⎆┊تـم بـدء تشغـيل سـورس ماتركـس الخاص بك .. بنجاح 🧸♥️**",
+                buttons=[(Button.url("ماتركــس𓅛", "https://t.me/veevvw"),)],
             )
     except Exception as e:
         LOGS.error(e)
@@ -364,9 +356,9 @@ async def verifyLoggerGroup():
             )
     else:
         descript = "لا تقم بحذف هذه المجموعة أو التغيير إلى مجموعة عامه (وظيفتهـا تخزيـن كـل سجـلات وعمليـات البـوت.)"
-        photozed = await zedub.upload_file(file="zedthon/malath/Zpic.jpg")
+        photozed = await zedub.upload_file(file="BiLaL/malath/Zpic.jpg")
         _, groupid = await create_supergroup(
-            "مجمـوعـة السجـل زدثـــون", zedub, Config.TG_BOT_USERNAME, descript, photozed
+            "مجمـوعـة السجـل ماتركـس", zedub, Config.TG_BOT_USERNAME, descript, photozed
         )
         addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
         print("تم إنشاء مجموعة السجل .. بنجاح ✅")
@@ -391,7 +383,7 @@ async def verifyLoggerGroup():
             LOGS.error("حدث خطأ اثناء التعرف على فار PM_LOGGER_GROUP_ID.\n" + str(e))
     else:
         descript = "لا تقم بحذف هذه المجموعة أو التغيير إلى مجموعة عامه (وظيفتهـا تخزيـن رسـائل الخـاص.)"
-        photozed = await zedub.upload_file(file="zedthon/malath/Apic.jpg")
+        photozed = await zedub.upload_file(file="BiLaL/malath/Apic.jpg")
         _, groupid = await create_supergroup(
             "مجمـوعـة التخـزيـن", zedub, Config.TG_BOT_USERNAME, descript, photozed
         )
